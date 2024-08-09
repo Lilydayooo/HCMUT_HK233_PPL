1b7ec164c465fabc8bec1980940722e5ba2f4aa4
LeTranKhanh <yoontrankhanh@gmail.com>
2024-08-09 11:58:44 +0700

    Added some functions

--- Ass3/src/main/mt22/checker/StaticCheck.py
+++ Ass3/src/main/mt22/checker/StaticCheck.py
@@ -92,4 +92,75 @@ class LookUp:
         if name in dict: f()
 
 class StaticChecker(Visitor):
-    pass
+    global_env = [
+        Sym("readInteger", MTyp([], IntegerType())),
+        Sym("printInteger", MTyp([IntegerType()],VoidType())),
+        Sym("readFloat", MTyp([], FloatType())),
+        Sym("writeFloat", MTyp([FloatType()], VoidType())),
+        Sym("readBoolean", MTyp([], BooleanType())),
+        Sym("printBoolean", MTyp([BooleanType()], VoidType())),
+        Sym("readString", MTyp([], StringType())),
+        Sym("printString", MTyp([StringType()], VoidType())),
+        Sym("super", MTyp([[Expr()]], VoidType())),
+        Sym("preventDefault", MTyp([], VoidType())),
+    ]
+
+    def __init__(self, ast):
+        self.ast = ast
+        self.env = [{}]
+        self.il_arr_lit = None
+        self.f_decl = {"flag": False, "return_type": None, "name": None, "inherit": {"func": None, "super_or_preventDefault": None}, "has_first_stmt_return": False}
+        self.loop = {"flag": False, "ast": None}
+        self.ifstmt = []
+        self.forstmt = []
+        self.whilestmt = []
+        self.do_whilestmt = []
+
+    def addIfEle(self, el): self.ifstmt.append(el)
+    def addForEle(self, el): self.forstmt.append(el)
+    def addWhileEle(self, el): self.whilestmt.append(el)
+    def addDoWhileEle(self, el): self.do_whilestmt.append(el)
+
+    def removeIfEle(self): self.ifstmt.pop()
+    def removeForEle(self): self.forstmt.pop()
+    def removeWhileEle(self): self.whilestmt.pop()
+    def removeDoWhileEle(self): self.do_whilestmt.pop()
+
+    def getIfSize(self): return len(self.forstmt)
+    def getForSize(self): return len(self.forstmt)
+    def getWhileSize(self): return len(self.whilestmt)
+    def getDoWhileSize(self): return len(self.do_whilestmt)
+
+    def set_loop(self, flag, ast):
+        self.loop["flag"] = flag
+        self.loop["ast"] = ast
+
+    def reset_loop(self):
+        self.loop["flag"] = False
+        self.loop["ast"] = None
+
+    def setFunctionDecl(self, flag, return_type, name):
+        self.f_decl["flag"] = flag
+        self.f_decl["return_type"] = return_type
+        self.f_decl["name"] = name
+
+    def resetFunctionDecl(self):
+        self.f_decl = {"flag": False, "return_type": None, "name": None, "inherit": {"func": None, "super_or_preventDefault": None}, "has_first_stmt_return": False}
+
+    def checking(self): return self.visit(self.ast, StaticChecker.global_env)
+
+    def raise_(self, err): raise err
+
+    def checkInherit(self, name, o, typ):
+        if not TUtils.noneCheck(self.f_decl["inherit"]["func"]) and self.f_decl["inherit"]["super_or_preventDefault"] == "super":
+            f = LookUp.lookup(
+                self.f_decl["inherit"]["func"]["name"], o, None, Function
+            )
+            par = self.lookup(
+                name, f["params_inherit"], lambda par:par["name"]
+            )
+            if not TUtils.noneCheck(par) and par["inherit"]:
+                self.raise_(Invalid(Parameter(), name))
+    
+    def visitProgram(self, ast: Program, c):
+        print(a)
